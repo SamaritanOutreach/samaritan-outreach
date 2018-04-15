91890f4de1b7c87889b2c8322ecce01f2271ec40
Bradley Winsler <bradleywinsler@Bradleys-MacBook-Pro.local>
2018-04-15 13:26:10 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Samaritan Outreach website',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-source-filesystem`,
+    `gatsby-plugin-netlify-cms`
+  ],
+}
